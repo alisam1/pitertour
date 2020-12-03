36a550efa68acda0491e15167f3b3b97869fcef7
Mariya <alisa-m2@yandex.ru>
2020-12-03 16:21:28 +0300

    Summary

---
+++ build/js/check.js
@@ -0,0 +1,11 @@
+function validate() {
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  var address = document.getElementById("email").value;
+  if (reg.test(address) == false) {
+    document.getElementById("password").disabled = true;
+    return false;
+  } else {
+    document.getElementById("password").disabled = false;
+    console.log(123);
+  }
+}
